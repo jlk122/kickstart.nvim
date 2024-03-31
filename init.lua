5d1f68a7fb4b27b4914d34c89ece31b833b7ff3e
Lukas Karlsson <johnlukas.k@gmail.com>
2024-03-31 22:30:56 +0200

    Added keymaps for spliting workspace

--- init.lua
+++ init.lua
@@ -962,6 +962,7 @@ require('lazy').setup({
     },
   },
 })
+require('lukas.keymaps')
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lukas/keymaps.lua
@@ -0,0 +1,2 @@
+vim.keymap.set('n', '<leader>pv', ':vsplit<CR>', { desc = 'S[p]lit window [v]ertically' })
+vim.keymap.set('n', '<leader>ph', ':split<CR>', { desc = 'S[p]lit window [h]orizontally' })
