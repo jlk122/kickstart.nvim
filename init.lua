236c96bfa9633ac8de75c3a0167c5b971446a5c6
Lukas Karlsson <johnlukas.k@gmail.com>
2025-10-06 09:49:57 +0200

    added clangformat, new search keybinds

--- init.lua
+++ init.lua
@@ -402,6 +402,9 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>s"', builtin.registers, { desc = '[S]earch Registers' })
+      vim.keymap.set('n', '<leader>sg', builtin.lsp_references, { desc = '[S]earch References' })
+      vim.keymap.set('n', '<leader>st', builtin.lsp_references, { desc = '[S]earch Treesitter' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
@@ -690,6 +693,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        -- cpp = { 'clang-format' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
